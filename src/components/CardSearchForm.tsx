8299c622fad694c0685b3cb78dbbc79ed1cd1702
Brian P. Adams <bpeteradams@gmail.com>
2022-03-17 09:26:51 -0400

    Scryfall(Search): Adjusted the SearchForm component to handle the getCardByName function onSubmit

--- src/components/CardSearchForm.tsx
+++ src/components/CardSearchForm.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { getCardByName } from '../utils/scryfall'
+
 
 export default function CardSearchForm() {
-	return (<form>
-		<input type="text" />
+	const [textValue, setTextValue] = useState('Mountain')
+	async function searchCard(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+		console.log(e)
+		const input = document.getElementById('card-name')
+		console.log(input)
+		const result = await getCardByName(textValue)
+		console.log(result);
+	}
+	return (<form onSubmit={searchCard}>
+		<input type="text" id='card-name' value={textValue} onChange={(e) =>{
+			const value = e.target.value
+			setTextValue(value);
+		}}/>
 		<button type='submit'>Search</button>
-	</form>)
+	</form >)
 }
